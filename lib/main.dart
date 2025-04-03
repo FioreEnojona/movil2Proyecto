9313962d55e0f3dafe7ee6d44ed0e26f24182a0c
FioreEnojona <estoyaquiporobligacion@gmail.com>
2025-04-02 21:41:58 -0600

    dsa

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movil2proyecto/login.dart';
 import 'package:movil2proyecto/Home_Screen.dart';
 import 'package:movil2proyecto/editar_perfil.dart';
+import 'package:movil2proyecto/menu.dart';
 import 'package:movil2proyecto/verReceta.dart';
 import 'package:movil2proyecto/registrar.dart';
 import 'package:movil2proyecto/notificaciones.dart';
@@ -27,9 +28,7 @@ class MyApp extends StatelessWidget {
         '/Busqueda': (context) => const BusquedaScreen(),
         '/editar_perfil': (context) => const EditarPerfilScreen(),
         '/Ver_Recetas': (context) => const VerRecetaPage(),
-        '/Registrar': (context) => const Registrar(),
         '/Notificaciones': (context) => const Notificaciones(),
-        '/Busqueda': (context) => const BusquedaScreen(),
         '/Recetas_Otros': (context) => const RecetasOtros(),
       },
     );
