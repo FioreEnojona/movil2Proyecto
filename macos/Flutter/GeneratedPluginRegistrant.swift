cb1e21c59db2f1c23f7b4c44dc8d8f19ff7bd61e
FioreEnojona <estoyaquiporobligacion@gmail.com>
2025-04-07 20:17:55 -0600

    Revert "Merge branch 'bryan' into romero"
    This reverts commit 60c7c702cdf01f381754d8bab36c12a503f664fe, reversing
    changes made to 63fef08d7c0213c5097f19ef1d4c12f39ce678df.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-=======
-import sqflite_darwin
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> bryan
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
